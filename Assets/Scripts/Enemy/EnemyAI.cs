bfb600d7b55f1d4c218978166da05335c9f801d2
hesenbg <hasn50875@gmail.com>
2025-10-15 12:37:29 +0400

    splitting up classes

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -3,7 +3,7 @@ using Unity.Mathematics;
 using UnityEngine;
 
 
-public class EnemyAI : MonoBehaviour 
+public class EnemyAI : MonoBehaviour
 {
     // === External References ===
     private GameObject player;
@@ -11,22 +11,22 @@ public class EnemyAI : MonoBehaviour
     private GuardSight guardSight;
     [HideInInspector] public GroundCheking leftChecker;
     [HideInInspector] public GroundCheking rightChecker;
-    [HideInInspector] public  EnemyAnimationManager AnimationManager;
+    [HideInInspector] public EnemyAnimationManager AnimationManager;
 
     // === Health & Combat ===
     private float damagePushVelocity = 1f;
     private float stunDuration = 1.5f;
 
     // === Movement ===
-    private float idleSpeed = 2.5f;
-    private float chaseSpeed = 4f;
-    private float moveDirection = 1f;
+    [SerializeField] float idleSpeed = 2.5f;
+    [SerializeField] float chaseSpeed = 4f;
+    [SerializeField] float moveDirection = 1f;
 
     // === State Management ===
-    public enum State { Wander, Fighting, Stunned }
+    public enum State { Wander, Fighting, Stunned, Chasing }
     public State currentState = State.Wander;
 
-    public enum EnemyType { ranged , melee }
+    public enum EnemyType { ranged, melee }
     public EnemyType CurrentEnemyType;
 
     // === Flags ===
@@ -38,10 +38,16 @@ public class EnemyAI : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         guardSight = GetComponent<GuardSight>();
 
+        leftChecker = GameObject.Find("LGroundCheking").GetComponent<GroundCheking>();
+        rightChecker = GameObject.Find("RGroundCheking").GetComponent<GroundCheking>();
 
     }
 
-    
+    private void Update()
+    {
+        UpdateAI();
+    }
+
     public void UpdateAI()
     {
         HandleTransitions();
@@ -75,39 +81,64 @@ public class EnemyAI : MonoBehaviour
             case State.Fighting:
                 Fight();
                 break;
+            case State.Stunned:
+                Stun();
+                break;
+            case State.Chasing:
+                Chase();
+                break;
         }
     }
-    
+
+    void Stun()
+    {
+
+    }
+
+    void Chase()
+    {
+
+    }
+
     private void Wander()
     {
         // flip direction when edges are reached
         if (!rightChecker.IsPressing && leftChecker.IsPressing)
+        {
             moveDirection *= -1;
+            RotateEnemy();
+        }
         else if (rightChecker.IsPressing && !leftChecker.IsPressing)
+        {
             moveDirection *= 1;
+            RotateEnemy();
+        }
 
-        rb.linearVelocityX = idleSpeed * moveDirection;
+        rb.linearVelocity = idleSpeed * transform.right.normalized;
     }
 
     private void Fight()
     {
         if (player == null) return;
-        if(CurrentEnemyType == EnemyType.melee)
+        if (CurrentEnemyType == EnemyType.melee)
         {
             moveDirection = math.sign(player.transform.position.x - rb.transform.position.x);
             rb.linearVelocityX = chaseSpeed * moveDirection;
         }
-        else if(CurrentEnemyType == EnemyType.ranged)
+        else if (CurrentEnemyType == EnemyType.ranged)
         {
             CanAttackAble = true;
         }
     }
 
-
-
-
-
-
+    void RotateEnemy()
+    {
+        float NewY = 0;
+        NewY += 180;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x,
+            NewY,
+            transform.eulerAngles.z);
+    }
 
     // === Visual Direction ===
     public float GetDirection() => moveDirection;
