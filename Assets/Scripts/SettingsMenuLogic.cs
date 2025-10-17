5e1ae79e4f2c1b5cd4fa60e3e7fd15bd3263d508
hesenbg <hasn50875@gmail.com>
2025-10-18 00:15:42 +0400

    settings menu

---
+++ Assets/Scripts/SettingsMenuLogic.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SettingsMenuLogic : MonoBehaviour
+{
+
+    [SerializeField] Toggle FullScreen;
+
+    [SerializeField] Slider Sound;
+
+    [SerializeField] AudioMixer mainMixer;
+
+
+    private void Update()
+    {
+        
+        ToggleFullscreen();
+        SetMasterVolume(Sound.value);
+
+    }
+    public void SetMasterVolume(float volume)
+    {
+        mainMixer.SetFloat("VFXVolume", Mathf.Log10(volume) * 20);
+    }
+
+    public void ToggleFullscreen()
+    {
+        Screen.fullScreen = FullScreen.isOn;
+    }
+
+    public void BackMainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+}
